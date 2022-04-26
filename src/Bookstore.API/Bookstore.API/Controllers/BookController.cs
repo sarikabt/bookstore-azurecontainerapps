9a6632eeb77887352c3bbd76a0944ae17050a6a5
Will Velida <will.velida@microsoft.com>
2022-04-26 16:28:01 +1200

    Adding API project to repo

---
+++ src/Bookstore.API/Bookstore.API/Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
